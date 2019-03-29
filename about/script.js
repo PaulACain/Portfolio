79027c137e3d3593ed0a9185edf2ecfb4c63595e
Paul <paul.audet.cain@gmail.com>
2019-03-29 13:10:35 -0400

    menu button

--- about/script.js
+++ about/script.js
@@ -1,9 +1,25 @@
 
-function myFunction(x) {
-  x.classList.toggle("change");
-}
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+};
 
+function mFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
 
 document.getElementById('change-theme-btn').addEventListener('click', function() {
   document.body.classList.toggle('dark-theme');
 });
+
+
+//function myFunction(x) {
+  x.classList.toggle("change");
+};
